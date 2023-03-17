ee078eb05e6a525861c2bb64f3b4ba535a6455aa
Isidro Bata <bata.isidro@gmail.com>
2023-03-17 18:45:20 +0200

    Print numbers using ascii

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	for (; i <= 57;)
+		putchar(i++);
+
+	putchar('\n');
+
+	return (0);
+}
