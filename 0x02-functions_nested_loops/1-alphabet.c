d7f97293939f1a009c78f6ee55e7abce8d98d230
Isidro Bata <bata.isidro@gmail.com>
2023-03-21 19:11:32 +0200

    Alphabet lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - print alphabet
  *
- * Return: void
+ * 
  */
 
 void print_alphabet(void)
