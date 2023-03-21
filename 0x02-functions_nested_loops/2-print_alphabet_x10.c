cc52f7283e278982aff9aa7474afb9da84644f20
Isidro Bata <bata.isidro@gmail.com>
2023-03-21 19:52:52 +0200

    Print x10 alphabet lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - This function print the alphabet x10
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 10;
+	int c;
+
+	while (i-- >= 0)
+	{
+		c = 97;
+		while (c <= 122)
+			_putchar(c++);
+		_putchar('\n');
+	}
+}
