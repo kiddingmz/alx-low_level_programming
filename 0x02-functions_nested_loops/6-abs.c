f74f59c85162d4b33d4cfd4af014f4c00af220e5
Isidro Bata <bata.isidro@gmail.com>
2023-03-21 23:04:20 +0200

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Absolute value of an integer
+ *
+ * @r: Receive the number to get absolute value
+ *
+ * Return: absolute number
+ */
+
+int _abs(int r)
+{
+	return ((r < 0) ? (-1 * r) : r);
+}
