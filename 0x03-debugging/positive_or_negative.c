72fca089e9a1a926e28ff893cb8fafe12c0ecaba
Isidro Bata <bata.isidro@gmail.com>
2023-03-22 18:07:30 +0200

    New print negative or positive

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - Print numbers
+ *
+ */
+
+void positive_or_negative(int n)
+{
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is positive\n", n);
+	}
+}
