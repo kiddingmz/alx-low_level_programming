6e087a832628e33369f521f18f918a1fe10a0902
Isidro Bata <bata.isidro@gmail.com>
2023-03-23 23:10:36 +0200

    Update file 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,16 +10,16 @@
 
 int largest_number(int a, int b, int c)
 {
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 		return (a);
 	}
-	else if (b > a && b > c)
-	{
-		return (b);
-	}
-	else if (c > a && c > b)
+	else if (c >= a && c >= b)
 	{
 		return (c);
+	}
+	else
+	{
+		return (b);
 	}
 }
