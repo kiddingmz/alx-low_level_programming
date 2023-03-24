9404fe1f2bcddd8e658d5777eddb7163032b7c87
Isidro Bata <bata.isidro@gmail.com>
2023-03-24 22:33:48 +0200

    Print numbers, 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int c = 48;
+
+	for (; c <= 57;)
+		_putchar(c++);
+	_putchar('\n');
+}
