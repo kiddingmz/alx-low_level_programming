30a50541e165c5769ea91db15f4e7ce2d8970c06
Isidro Bata <bata.isidro@gmail.com>
2023-03-24 22:23:09 +0200

    Check if is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - Chech a digit
+ *
+ * @c: Param integer
+ *
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
