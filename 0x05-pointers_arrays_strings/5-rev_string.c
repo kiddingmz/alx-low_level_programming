cd93dabacb9ec2c2179b87343f94c0909b17af6e
Isidro Bata <bata.isidro@gmail.com>
2023-03-28 12:56:26 +0200

    Reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse string
+ *
+ * @s: String
+ *
+ */
+
+void rev_string(char *s)
+{
+	int u = 0, c = 0;
+	char *t = s, n;
+
+	while (*t != '\0')
+	{
+		c++;
+		t++;
+	}
+
+	while (u++ < c--)
+	{
+		n = *(s + u);
+		*(s + u) = *(s + c);
+		*(s + c) = n;
+	}
+}
