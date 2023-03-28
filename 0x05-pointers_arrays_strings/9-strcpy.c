d4fbfd1a08ff50553627b1ad2ee8e311d9a9a28f
Isidro Bata <bata.isidro@gmail.com>
2023-03-28 15:48:09 +0200

    String copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy the string pointed to by src
+ *
+ * @dest: Destin
+ * @src: Source
+ *
+ * Return: String
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *s = src;
+	int c = 0;
+
+	for (; *s != '\0'; c++, s++)
+		*(dest + c) = *(src + c);
+
+	dest[++c] = '\0';
+	return (dest);
+}
