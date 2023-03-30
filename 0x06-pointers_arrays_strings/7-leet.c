1f4bbd205fdedf335a30d095a31b824bdc4a6a54
Isidro Bata <bata.isidro@gmail.com>
2023-03-30 21:02:30 +0200

    Leet - 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * lett - Encodesa string
+ *
+ * @s: String
+ *
+ * Return: String
+ *
+ */
+
+char *leet(char *s)
+{
+	int i, c = 0;
+	int lcase[] = {97, 101, 111, 116, 108};
+	int ucase[] = {65, 69, 79, 84, 76};
+	int n[] = {52, 51, 48, 55, 49};
+
+	while (s[c] != '\0')
+	{
+		for (i = 0; i <= 4; i++)
+		{
+			if (s[c] == lcase[i] || s[c] == ucase[i])
+			{
+				s[c] = n[i];
+				break;
+			}
+		}
+		c++;
+	}
+	return (s);
+}
