abd17a8b879c5a1b03ea0631b5f056e845c06611
Isidro Bata <bata.isidro@gmail.com>
2023-03-30 15:00:48 +0200

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,24 +11,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0, c = 0, ls1 = 0, ls2 = 0;
+	int i = 0, c = 0;
 
-	while (s1[i] != '\0' && s2[j] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] == s2[i])
-			c++;
-		else if (s1[i] > s2[j])
-			ls1++;
-		else if (s2[i] > s1[j])
-			ls2++;
+			c = s1[i] - s2[i];
+		else
+		{
+			c = s1[i] - s2[i];
+			break;
+		}
 		i++;
-		j++;
 	}
-
-	if (c == i && c == j)
-		return (0);
-	else if (ls1 > ls2)
-		return (15);
-	else
-		return (-15);
+	return (c);
 }
