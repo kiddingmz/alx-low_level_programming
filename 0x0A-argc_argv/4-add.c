46d7bf86b732c4a3a48da851c6f591156e92a515
Isidro Bata <bata.isidro@gmail.com>
2023-04-11 17:12:45 +0200

    Sum

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: number of arguments
+ * @argv: vetor of string
+ *
+ * Return: 0 - sucess or 1 - fail
+ */
+
+int main(int argc, char *argv[])
+{
+	long int sum;
+	int i;
+
+	sum = 0l;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else if (argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else if (atoi(argv[i]) > 0)
+			sum += atoi(argv[i]);
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
