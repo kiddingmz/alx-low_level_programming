f0ed505ee2f5554b309a3ea8fedb6fb296574ddf
Isidro Bata <bata.isidro@gmail.com>
2023-04-11 17:33:36 +0200

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,22 +15,21 @@ int main(int argc, char *argv[])
 	int sum, i;
 
 	if (argc == 1)
-	{
 		printf("0\n");
-		return (0);
-	}
-
-	sum = 0;
-	for (i = 1; i < argc; i++)
+	else
 	{
-		if (atoi(argv[i]) == 0)
+		sum = 0;
+		for (i = 1; i < argc; i++)
 		{
-			printf("Error\n");
-			return (1);
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum += atoi(argv[i]);
 		}
-		else
-			sum += atoi(argv[i]);
+		printf("%d\n", sum);
 	}
-	printf("%d\n", sum);
 	return (0);
 }
