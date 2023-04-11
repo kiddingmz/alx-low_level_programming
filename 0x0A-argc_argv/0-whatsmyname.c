a014a9048b7fb92422f2edf74c8308ba6f5c0470
Isidro Bata <bata.isidro@gmail.com>
2023-04-11 15:36:24 +0200

    Print name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: number of arguments
+ * @argv: vetor of string
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	char *arg = argv[argc - argc];
+	int i;
+
+	for (i = 0; arg[i] != '\0'; i++)
+		_putchar(arg[i]);
+	
+	_putchar('\n');
+	return (0);
+}
