378f0231da9c94a8437272145ce862203aba97b3
Isidro Bata <bata.isidro@gmail.com>
2023-04-18 23:47:38 +0200

    New dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include "dog.h"
+
+/**
+ * new_dog - create new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner
+ *
+ * Return: new_dog or NULL;
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		return (NULL);
+	}
+	(*d).name = strdup(name);
+	if ((*dog).name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	(*dog).owner = strdup(owner);
+	if ((*dog).owner == NULL)
+	{
+		free((*dog).name);
+		free(dog);
+		return (NULL);
+	}
+	(*dog).age = age;
+	return (dog);
+}
