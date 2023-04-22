920fffcca37abaa48d1f79365aa12e1465f38284
Isidro Bata <bata.isidro@gmail.com>
2023-04-22 16:41:26 +0200

    Cal

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-cal.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: number of params
+ * @argv: params
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	char *c;
+	int n1, n2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	
+	c = argv[2];
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+
+	if (get_op_func(c) == NULL || c[1] != '\0')
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((*c == '/' || *c == '%') && n2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	printf("%d\n", get_op_func(c)(n1, n2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,72 @@
+#include "3-cal.h"
+
+/**
+ * op_add - sum a and b
+ *
+ * @a: firt integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference a and b
+ *
+ * @a: firt integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
+ */
+ 
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of a and b
+ *
+ * @a: firt integer
+ * @b: second integer
+ *
+ * Return: product of a and b
+ */
+ 
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of a and b
+ *
+ * @a: firt integer
+ * @b: second integer
+ *
+ * Return: division of a and b
+ */
+ 
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division of a and b
+ *
+ * @a: firt integer
+ * @b: second integer
+ *
+ * Return: remainder of a and b
+ */
+ 
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
