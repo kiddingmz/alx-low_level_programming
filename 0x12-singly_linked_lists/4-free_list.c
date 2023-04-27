49ea36798dc45cd3a843788ca78bdff9c8d456c6
Isidro Bata <bata.isidro@gmail.com>
2023-04-27 21:37:30 +0200

    frees valgrind

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_lists - frees a list_t
+ *
+ * @head: struct
+ *
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *aux;
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		aux = head;
+		head = head->next;
+		free(aux);
+	}
+	free(head);
+}
