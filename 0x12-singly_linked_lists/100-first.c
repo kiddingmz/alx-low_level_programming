c028aeab10674493e346d4512d2ea1fa05207751
Isidro Bata <bata.isidro@gmail.com>
2023-04-27 21:59:28 +0200

    execute before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * myPrintf - printf before main
+ *
+ * Return: nothing
+ */
+
+void myPrintf(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
