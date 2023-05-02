c1f5218cc1004d0c52fb2e614f83ddc5e3ebde77
Isidro Bata <bata.isidro@gmail.com>
2023-05-02 21:11:44 +0200

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new->n = n;
 	new->next = NULL;
-	if ((*head) == NULL && idx - 1 == 0)
+	if ((*head) == NULL && idx == 0)
 		(*head) = new;
 	else
 	{
@@ -34,6 +34,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		if (aux == NULL)
 			return (NULL);
+		if (idx == 0)
+		{
+			new->next = (*head);
+			*head = new;
+			return (new);
+		}
 		new->next = aux;
 		tmp->next = new;
 	}
