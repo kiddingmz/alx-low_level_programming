13f1570aeb6e959868e45c4ecfb18b6a207ef06e
Isidro Bata <bata.isidro@gmail.com>
2023-05-02 20:14:55 +0200

    update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	i = 0;
-	while (head != NULL && i++ <= index - 1)
+	while (head != NULL && i++ < index)
 		head = head->next;
 
 	if (head != NULL)
