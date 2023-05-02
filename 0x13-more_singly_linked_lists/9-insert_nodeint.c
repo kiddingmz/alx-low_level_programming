8d4344749f4e70bba41bde553b59d140f62d9913
Isidro Bata <bata.isidro@gmail.com>
2023-05-02 20:41:23 +0200

    add index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - add a new node give position
+ *
+ * @head: struct
+ * @idx: index
+ * @n: element
+ *
+ * Return: listint_t
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *aux, *tmp;
+	unsigned int i;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+	if ((*head) == NULL && idx - 1 == 0)
+		(*head) = new;
+	else
+	{
+		aux = *head;
+		i = 0;
+		while (aux != NULL && i++ < idx)
+		{
+			tmp = aux;
+			aux = aux->next;
+		}
+		if (aux == NULL)
+			return (NULL);
+		new->next = aux;
+		tmp->next = new;
+	}
+	return (new);
+}
