0b48df92a0931c8b3683300734e494b5220a6c4d
Isidro Bata <bata.isidro@gmail.com>
2023-05-02 21:56:03 +0200

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,7 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			*head = new;
 			return (new);
 		}
-		new->next = aux;
+		new->next = aux->next;
 		tmp->next = new;
 	}
 	return (new);
