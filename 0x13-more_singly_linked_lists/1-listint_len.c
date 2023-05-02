c1581f478a31dc92ab684337ff439805efd6cd50
Isidro Bata <bata.isidro@gmail.com>
2023-05-02 17:32:34 +0200

    len int

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - Return numbers of the elements in a linked list
+ *
+ * @h: linked list
+ *
+ * Return: unsigned int
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t len;
+
+	len = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
+
