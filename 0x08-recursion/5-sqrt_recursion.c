0f1742d4792af05a581920d06107b411befff080
Isidro Bata <bata.isidro@gmail.com>
2023-05-03 07:52:46 +0200

    update

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * power_operation - natural square root of a number
+ *
+ * @n: integer
+ * @c: iterator
+ *
+ * Return: integer
+ */
+
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+/**
+ * _sqrt_recursion - natural square root of a number
+ *
+ * @n: integer
+ *
+ * Return: intger
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
+int _putchar(char);
+void _puts_recursion(char *);
+void _print_rev_recursion(char *);
+int _strlen_recursion(char *);
 int factorial(int n);
-int _pow_recursion(int x, int y);
+int _pow_recursion(int, int);
+int _sqrt_recursion(int);
+int is_prime_number(int);
 
 #endif
