5e5e0b3b18c5985a35f674f4d026f8221eda5eb1
Isidro Bata <bata.isidro@gmail.com>
2023-05-03 08:58:43 +0200

    update

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory area
+ *
+ * @dest: destine
+ * @src: source
+ * @n: unsigned int
+ *
+ * Return: String
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int c;
+
+	for (c = 0; i < n; c++)
+		dest[c] = src[c];
+	return (dest);
+}
