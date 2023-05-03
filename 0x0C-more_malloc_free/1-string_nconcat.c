65ab321b3ee9cc05c54a9317f47527835a939d9c
Isidro Bata <bata.isidro@gmail.com>
2023-05-03 07:03:56 +0200

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,9 +3,11 @@
 
 /**
  * string_nconcat - concatenates two strings
- * @s1: first string 
+ *
+ * @s1: first string
  * @s2: second string
  * @n: bytes
+ *
  * Return: pointer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
