80c2bd512d00965dbc67cb8d28cd33ae351b9512
Isidro Bata <bata.isidro@gmail.com>
2023-05-04 22:25:58 +0200

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - print binary
+ *
+ * @n: decimal number
+ *
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int i, c;
+	unsigned long int decimal;
+
+	i = 63;
+	c = 0;
+	while (i--)
+	{
+		decimal = n >> i;
+		if (decimal & 1)
+		{
+			putchar('1');
+			c++;
+		}
+		else if (decimal)
+			putchar('0');
+	}
+	if (!decimal)
+		putchar('0');
+}	
