1d2af1e981e2b25e461f9ea1442b0fe3f6d3421a
Isidro Bata <bata.isidro@gmail.com>
2023-05-08 20:56:08 +0200

    udpate

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,11 +20,9 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
 	if (file == -1)
 		return (-1);
-	while (*text_content)
-	{
-		wc++;
-		*text++;
-	}
+	wc = 0;
+	while (text_content[wc++] != '\0')
+		;
 
 	w = write(file, text_content, wc);
 	if (w == -1)
