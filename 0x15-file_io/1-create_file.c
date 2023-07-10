5dc0f13ace6f78946ea7247017e06661dd4a1fdc
Isidro Bata <bata.isidro@gmail.com>
2023-07-10 09:19:18 +0200

    file i/o

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ *
+ * @filename: file
+ * @text_content: text content
+ *
+ * Return: 1 on sucess or -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t w, wc;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (file == -1)
+		return (-1);
+	wc = 0;
+	if (text_content != NULL)
+	{
+		while (text_content[wc] != '\0')
+			wc++;
+	}
+	w = write(file, text_content, wc);
+	if (w == -1)
+		return (-1);
+	close(file);
+	return (1);
+}
+
