f8695a9fa78181d949dd2f8c5e0aba93ac833883
kiddingmz <bata.isidro@gmail.com>
2023-12-13 18:39:25 +0200

    FEAT: 7. Jump search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -23,15 +23,8 @@ typedef struct listint_s
 } listint_t;
 
 
-/*
- * Function that searches for a value in an
- * array of integers using the Linear search algorithm
- */
 int linear_search(int *array, size_t size, int value);
-
-/*
- * Function that searches for a value in a sorted array
- * of integers using the Binary search algorithm
- */
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
 #endif
