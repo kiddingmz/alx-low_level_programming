26f992fa818556ab7b69dcab838707c54a69af43
kiddingmz <bata.isidro@gmail.com>
2023-12-13 18:42:46 +0200

    FEAT:  12. Jump search in a singly linked list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -29,5 +29,8 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+void print_list(const listint_t *list);
+listint_t *create_list(int *array, size_t size);
+void free_list(listint_t *list);
 
 #endif
